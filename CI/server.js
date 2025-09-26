c94c7544cd1ee8d99a06219ba721ed388f0c71fe
STIWARTs <stiwartsaxena477@gmail.com>
2025-09-26 17:25:14 +0530

    CI

---
+++ CI/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import sum from "./sum.js";
+
+const app = express();
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/home", async (req, res) => {  
+    res.json({ message: "I am root!" });
+});
+
+app.get("/getSum/:a/:b", async (req, res) => {
+    const { a, b } = req.params;
+
+    const sum = parseInt(a) + parseInt(b);
+    res.json({ sum });
+}); 
